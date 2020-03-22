9a842f8a22b31a46b0e8cc983fba63a338e753e3
Jesse Chan <jc@linux.com>
2020-03-22 20:41:09 +0800

    update plugins

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git submodule update --init --recursive
+
 ln -s /home/jc/.dotfiles/.oh-my-zsh/custom/plugins/zsh-autosuggestions /home/jc/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 ln -s /home/jc/.dotfiles/.oh-my-zsh/custom/plugins/fast-syntax-highlighting /home/jc/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
 ln -s /home/jc/.dotfiles/.oh-my-zsh/custom/plugins/zsh-256color /home/jc/.oh-my-zsh/custom/plugins/zsh-256color
@@ -8,4 +10,5 @@ mkdir /home/jc/.oh-my-zsh/functions
 ln -s /home/jc/.dotfiles/zsh-async/async.zsh /home/jc/.oh-my-zsh/functions/async
 ln -s /home/jc/.dotfiles/zsh_theme_theta-async/theta-async.zsh /home/jc/.oh-my-zsh/functions/prompt_theta_setup
 
+rm /home/jc/.zshrc
 ln -sf /home/jc/.dotfiles/.zshrc /home/jc/.zshrc
